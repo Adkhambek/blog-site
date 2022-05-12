ed6a568716c8d9cffdecb5363abe5f5abf3553e4
Adham <muzaffarov.adham@gmail.com>
2022-05-12 14:56:44 +0500

    some improvements

--- src/seeders/20220512093932-posts.js
+++ src/seeders/20220512093932-posts.js
@@ -1,7 +1,7 @@
 module.exports = {
     async up(queryInterface, Sequelize) {
         await queryInterface.bulkInsert(
-            "categories",
+            "posts",
             [
                 {
                     title: "This is my first blog post ever",
@@ -18,6 +18,6 @@ module.exports = {
     },
 
     async down(queryInterface, Sequelize) {
-        await queryInterface.bulkDelete("categories", null, {});
+        await queryInterface.bulkDelete("posts", null, {});
     }
 };
